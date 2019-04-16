5cd819669f94c88ac81b84a9317130b0291c75b4
Drissiaaa <drissiadevd@gmail.com>
2019-04-16 17:10:24 +0200

    About

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class AboutController extends AbstractController
+{
+
+    public function index()
+    {
+        return $this->twig->render('/About/about.html.twig'
+        );
+    }
+}
+
