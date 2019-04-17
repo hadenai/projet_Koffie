2ccb5e6e1317bb37fb85a3dc9264c489afc42f59
Drissiaaa <drissiadevd@gmail.com>
2019-04-17 14:29:42 +0200

    add section about

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,14 +3,11 @@
 
 namespace App\Controller;
 
-
 class AboutController extends AbstractController
 {
 
     public function index()
     {
-        return $this->twig->render('/About/about.html.twig'
-        );
+        return $this->twig->render('/About/about.html.twig');
     }
 }
-
