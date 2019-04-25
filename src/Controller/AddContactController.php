ee952845d0e82b5c88ee82a1bf8f8b8ebf0dd87f
Arthur Pralon <arthur.pralon@wanadoo.fr>
2019-04-25 10:18:03 +0200

    modification of add contact controller

--- src/Controller/AddContactController.php
+++ src/Controller/AddContactController.php
@@ -41,7 +41,7 @@ class AddContactController extends AbstractController
                     'yourmessage' => $_POST['yourmessage'],
                 ];
                 $id = $addContactManager->insert($contact);
-                header('Location:Home/contact.html.twig');
+                header('Location:/');
             }
         }
         return $this->twig->render('Home/addcontact.html.twig', [
